f3746bae6d9c173b6af72d33b73b0db87bdbb519
ehgud1008 <ehgud1008@naver.com>
2023-12-15 18:41:28 +0900

    헤더 세팅

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,39 +2,46 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <ul className='flex gap-4'>
+        {/* 햄버거 아이콘 */}
+        <div class="block lg:hidden">
+          <div id="burger" class="flex items-center" onclick="toggleMenu()">
+            <img src='/images/hamburger.png' className='w-7 h-7' alt='menu' />
+          </div>
+        </div>
+        <ul className='flex gap-4 hidden lg:flex absolute  top-0 right-0 mt-16 mr-4 bg-white shadow-md rounded-md'>
           <Link to="">
-            <li className='hidden sm:inline text-slate-700 hover:underline font-bold'>인사말</li>
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>인사말</li>
           </Link>
           <Link to="">
-            인터뷰
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>인터뷰</li>
           </Link>
           <Link to="">
-            웨딩데이
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>웨딩데이</li>
           </Link>
           <Link to="">
-            갤러리
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>갤러리</li>
           </Link>
           <Link to="">
-            예식정보
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>예식정보</li>
           </Link>
           <Link to="">
-            게스트북
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>게스트북</li>
           </Link>
           <Link to="">
-            연락처
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>연락처</li>
           </Link>
           <Link to="">
-            마음 전하실 곳
+            <li className='sm:inline text-slate-700 hover:underline font-bold'> 마음 전하실 곳</li>
           </Link>
           <Link to="">
-            오시는 길
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>오시는 길</li>
           </Link>
           <Link to="">
-            감사 인사
+            <li className='sm:inline text-slate-700 hover:underline font-bold'>감사 인사</li>
           </Link>
         </ul>
       </div>
