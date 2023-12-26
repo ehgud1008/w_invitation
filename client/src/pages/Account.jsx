feb8cac9d65f2602e6d0bbf60290e16ed8234ac2
ehgud1008 <ehgud1008@naver.com>
2023-12-27 00:06:17 +0900

    계좌 번호 페이지

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -14,8 +14,8 @@ const Account = () => {
     <div className="mt-10">
         <p className='mb-5 grid place-items-center '>마음 전하실 곳</p>
         <div className="grid place-items-center mb-3">
-            <div className="md:w-2/4 xs:w-2/3 border rounded overflow-hidden">
-                <div className="border-b bg-sky-50 py-2 cursor-pointer" onClick={toggleAccordion1}>
+            <div className="md:w-2/4 xs:w-4/5 rounded overflow-hidden">
+                <div className="border border-b bg-sky-50 py-2 cursor-pointer" onClick={toggleAccordion1}>
                     <div className="flex justify-between items-center">
                         <div className="flex items-center ml-3">
                             <span className="text-sm font-semibold flex items-center">
@@ -30,15 +30,25 @@ const Account = () => {
                 </div>
                 {isAccountOpen1 && (
                     <div className="p-2 text-sm">
-                        <p>아코디언 내용...</p>
+                        <div className='flex justify-between items-center'>
+                            <span>신한은행</span>
+                            <span className='text-xs'>예금주 : 김철수</span>
+                        </div>
+                        <div className='border-2 border-gray-300 px-3 py-2'>
+                            <div className='text-md flex justify-between items-center'> 
+                                <span>110-123-123456</span>
+                                <button className='border-slate-300 rounded-md bg-gray-500 text-white py-1 px-2'>복사</button>
+                            </div>
+                            
+                        </div>
                     </div>
                 )}
             </div>
         </div>
 
         <div className="grid place-items-center mb-3">
-            <div className="md:w-2/4 xs:w-2/3 border rounded overflow-hidden">
-                <div className="border-b bg-pink-50 py-2 cursor-pointer" onClick={toggleAccordion1}>
+            <div className="md:w-2/4 xs:w-4/5 border rounded overflow-hidden">
+                <div className="border-b bg-pink-50 py-2 cursor-pointer" onClick={toggleAccordion2}>
                     <div className="flex justify-between items-center">
                         <div className="flex items-center ml-3">
                             <span className="text-sm font-semibold flex items-center">
@@ -51,7 +61,7 @@ const Account = () => {
                         </div>
                     </div>
                 </div>
-                {isAccountOpen1 && (
+                {isAccountOpen2 && (
                     <div className="p-2 text-sm">
                         <p>아코디언 내용...</p>
                     </div>
